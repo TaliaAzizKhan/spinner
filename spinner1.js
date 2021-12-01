c04b3fc795218db4f552096f0a62cc5ec28c2315
Talia Aziz Khan <talyaaziz@gmail.com>
2021-12-01 05:37:08 +0000

    spinner1 added

---
+++ spinner1.js
@@ -0,0 +1,37 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+// ... fill in the rest yourself ...
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1200);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1400);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1600);
