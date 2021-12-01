f97cc5b2c9f0165b22396ea90570b0fcd5f10d3e
Talia Aziz Khan <talyaaziz@gmail.com>
2021-12-01 05:47:16 +0000

    spinner 1 refactored

---
+++ spinner2.js
@@ -0,0 +1,12 @@
+process.stdout.write('hello from spinner2.js... \rheyyy\n');
+
+let spinDelay = 100;
+
+let spinner = ["\r|   ", "\r/   ", "\r-   ", "\r\\   ", "\r|   ", "\r/   ", "\r-   ", "\r|   "];
+
+for (const r of spinner) {
+  setTimeout(() => {
+    process.stdout.write(r);
+  }, spinDelay)
+  spinDelay += 200;
+}
